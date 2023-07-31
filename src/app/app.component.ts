99d26091d47bc05c5bfe9cb79497ec7f5eaad9b0
icekombat <kakoschkedaniel@gmail.com>
2023-07-31 20:59:24 +0200

    first trys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,125 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {ClickMode, Container, Engine, HoverMode, MoveDirection, OutMode} from "tsparticles-engine";
+import {loadSlim} from "tsparticles-slim";
+import {FormBuilder, FormGroup} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'C-A-R';
+export class AppComponent implements OnInit{
+  title:string = 'C-A-R';
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  // @ts-ignore
+  form: FormGroup;
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  send(): void {
+    //send data to backend
+    const { name, email,country,company,number,goals } = this.form.value;
+  }
+
+  private buildForm(): void {
+    this.form = this.formBuilder.group({
+      name: this.formBuilder.control(null),
+      email: this.formBuilder.control(null),
+      country: this.formBuilder.control(null),
+      company: this.formBuilder.control(null),
+      number: this.formBuilder.control(null),
+      goals:this.formBuilder.control(null)
+    });
+  }
+
+  // Particle Background
+  id:string = "particles-bg";
+
+  particleOptions =
+    {
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: ClickMode.push,
+        },
+        onHover: {
+          enable: true,
+          mode: HoverMode.repulse,
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 100,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#fff",
+      },
+      links: {
+        color: "#fff",
+        distance: 170,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: MoveDirection.none,
+        enable: true,
+        outModes: {
+          default: OutMode.bounce,
+        },
+        random: false,
+        speed: 0.5,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 86,
+      },
+      opacity: {
+        value: 0.7,
+      },
+      shape: {
+        type: "none",
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+    },
+    detectRetina: true,
+  };
+
+  particlesLoaded(container: Container): void {
+    console.log(container);
+  }
+
+  async particlesInit(engine: Engine): Promise<void> {
+    console.log(engine);
+
+    // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    //await loadFull(engine);
+    await loadSlim(engine);
+  }
+
+  protected readonly console = console;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import {NgParticlesModule} from "ng-particles";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgParticlesModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
